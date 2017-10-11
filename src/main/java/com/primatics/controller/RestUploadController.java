dac3b97cc8ed203e283d7fe58dffe06290821208
rarun <rarun@primaticsfinancial.com>
2017-10-11 10:05:34 -0400

    updates

--- src/main/java/com/primatics/controller/RestUploadController.java
+++ src/main/java/com/primatics/controller/RestUploadController.java
@@ -19,10 +19,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +31,6 @@ import com.google.common.base.Stopwatch;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 import com.mongodb.gridfs.GridFSDBFile;
-import com.primatics.model.RawData;
 
 @RestController
 public class RestUploadController {
